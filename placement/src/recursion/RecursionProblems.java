979c8e0ef392d796f75ac834189f2ebde7527997
Piyush Jain <zenGhub@gmail.com>
2023-09-11 22:12:55 +0530

    updated code for recursion

--- placement/src/recursion/RecursionProblems.java
+++ placement/src/recursion/RecursionProblems.java
@@ -3,7 +3,8 @@ package recursion;
 public class RecursionProblems {
 
     public static void main(String[] args) {
-        printNum(10);
+      //  printNum(10);
+        System.out.println( sumRecursive(5));
 
     }
 
@@ -12,10 +13,26 @@ public class RecursionProblems {
     public static void printNum(int num) {
 
         if (num == 0) {
-            return;
+            return ;
         } else {
             System.out.println(num);
             printNum(num - 1);
         }
     }
+
+
+   // sum of N natural numbers using recursion
+
+    public static int sumRecursive(int N){
+
+        if(N==0){
+            return 0;
+        }
+        return N+sumRecursive(N-1);
+
+    }
+
+    // check for palindrome using recursion
+
+
 }
