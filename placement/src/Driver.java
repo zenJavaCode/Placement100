f2ba52131085236985d31e253d0f853555888158
piyushjain.p <piyush.j@moxey.ai>
2023-12-23 20:37:00 +0530

    update code for doubly linked list

--- placement/src/Driver.java
+++ placement/src/Driver.java
@@ -3,20 +3,26 @@ import linkedlist.Node;
 public class Driver {
 
     public static void main(String[] args) {
-        Node head = new Node(10);
-        Node temp1 = new Node(20);
-        Node temp2 = new Node(30);
-
-        head.next = temp1;
-        temp1.next = temp2;
-  //      recursiveTraversalLinkedList(head);
-        Node node = Node.insertAtTheGivenPositionOfLinkedList(head,25,2);
-        System.out.println(node);
+//        Node head = new Node(10);
+//        Node temp1 = new Node(20);
+//        Node temp2 = new Node(30);
+//
+//        head.next = temp1;
+//        temp1.next = temp2;
+//  //      recursiveTraversalLinkedList(head);
+//        Node node = Node.insertAtTheGivenPositionOfLinkedList(head,25,2);
+//        System.out.println(node);
         // print a linked list
 //        while(head !=null){
 //            System.out.println(head.data);
 //            head = head.next;
 //        }
+
+        linkedlist.doublyLinkedList.Node head = new linkedlist.doublyLinkedList.Node(10);
+
+        linkedlist.doublyLinkedList.Node temp1 =new linkedlist.doublyLinkedList.Node(20);
+
+
 
 
     }
---
+++ placement/src/linkedlist/doublyLinkedList/Node.java
@@ -0,0 +1,13 @@
+package linkedlist.doublyLinkedList;
+
+public class Node {
+
+    int data;
+    Node prev;
+    Node next;
+   public   Node(int d){
+        this.data=d;
+        prev=null;
+        next=null;
+    }
+}
