46839de21f3dc02b8256bfd295635b13b1fc474a
Piyush Jain <zenghub@gmail.com>
2024-02-02 00:49:36 +0530

    updated code array problems

--- placement/src/arrays/ArrayProblems.java
+++ placement/src/arrays/ArrayProblems.java
@@ -192,6 +192,14 @@ return array;
         * */
 
 
+
+
+        /*TODO:- Write a Program that takes N elements (max. value of N is 100
+                and N is the float number specified by user) from user, stores data
+                in an array and Calculates the average of those numbers.*/
+
+
+
 }
 
 
